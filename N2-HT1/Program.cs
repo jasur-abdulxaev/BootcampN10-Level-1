48af548796369ca561af7f507b821c060ac4ad94
jasur-abdulxaev <abdulkhaevjasur@gmail.com>
2026-01-23 14:35:45 +0500

    N2-HT1 added!

---
+++ N2-HT1/Program.cs
@@ -0,0 +1,50 @@
+﻿//HT-1
+Console.WriteLine("Home Tasks - 1");
+Console.Write("Enter your first name: ");
+string firstName = Console.ReadLine();
+
+Console.Write("Last name: ");
+string lastName = Console.ReadLine();
+
+Console.Write("Enter your age: ");
+byte age = byte.Parse(Console.ReadLine());
+
+Console.WriteLine($"\nFirst name: {firstName}\nLast name: {lastName}\nAge: {age}\n");
+
+//HT-2
+Console.WriteLine("Task - 2");
+DateTime today = DateTime.Now;
+var date = DateTime.UnixEpoch;
+byte ageX = 32;
+float balance = 35.5F;
+string name = "Max developer";
+
+Console.WriteLine(today);
+Console.WriteLine(date.ToString("yyyy dd MMMM"));
+Console.WriteLine(ageX);
+Console.WriteLine(balance);
+Console.WriteLine(name);
+
+// HT-3
+Console.WriteLine("\nTask - 3");
+//Primitive
+bool isAlive = false;
+int price = 184_000;
+
+Console.WriteLine($"Primitive types: {isAlive}, {price}");
+
+//Complex
+DateTime now = DateTime.Now;
+
+Console.WriteLine($"Copmlex type {now}");
+
+//Value type
+char letter = 'A';
+int population = 37_225_000;
+
+Console.WriteLine($"Value types: {letter}, {population}");
+
+//Reference types
+string modelName = "HP Victus";
+
+Console.WriteLine($"Reference types: {modelName}");
--- N2/Program.cs
+++ N2/Program.cs
@@ -36,3 +36,110 @@ Console.WriteLine(piDecimal);
 Console.WriteLine(firstName);
 
 #endregion
+
+#region Definition and Initialization
+
+// Definition - E'lon qilish
+byte ageB;
+//Console.WriteLine(ageB);
+
+//DO SOMETHING
+
+//Initialization - Qiymat berish
+ageB = 21;
+#endregion
+
+#region Read and Write
+
+int speedA = 100;
+int speedB;
+
+// Write operation for speed B - speed B ga yozish operatsiyasi
+speedB = speedA;
+
+// Read operation for speed A - speed A esa o'qish operatsiyasi
+speedA = speedB;
+
+#endregion
+
+#region Explicit and Implicity typing
+
+//Implicit - bu yerda e'lon qilinishida farqi implicitda o'zi aniqlaydi
+var lastName = "John";
+
+// Type doesn't change
+//lastName = true;
+
+//Initialization is must
+var ageX = 12;
+//var fullName;
+//fullName = firstName + lastName;
+
+// Explicit
+int ageZ = 10;
+
+#endregion
+
+#region Literals or Const value - Literallar bular code ichida elon qilinadigan o'zgaruvchilar ya'ni konstantalar
+
+var boolValue = false;
+var charValue = '@';
+var byteValue = (byte)10;
+var sbayteValue = (sbyte)10;
+var intValue = 10;
+var uintValue = 10U;
+var shortValue = (short)10;
+var ushortValue = (ushort)10;
+var longValue = 10L;
+var ulongValue = 10UL;
+var floatValue = 10F;
+var doubleValue = 10D;
+var decimalValue = 10M;
+var stringValue = "Bob";
+
+Console.ForegroundColor = ConsoleColor.Green;
+
+//Default value
+int price = default;
+var priceB = default(int);
+Console.WriteLine($"Price: {price}");
+bool defaultValue = true;
+defaultValue = default;
+
+string defaultStringValue = default;
+string defaultStringValueB = string.Empty;
+
+Console.WriteLine(defaultValue);
+
+#endregion
+
+#region Primitive and Complex types
+// Primitive 
+var productName = "Headphones";
+
+// Complex
+DateTime now = DateTime.Now;
+Console.WriteLine(now);
+
+#endregion
+
+#region Value and Reference types - Xotirani(RAM) qayeridan joy egallashiga qarab
+
+//Value types
+bool isWhite = false;
+
+// Reference type
+var address = "Alisher NAvoiy ko'chasi, 23";
+
+#endregion
+
+#region Static and Dinamic
+
+// Static 
+var modelNumber = "34567890";
+
+//Dynamic
+
+#endregion
+
+
