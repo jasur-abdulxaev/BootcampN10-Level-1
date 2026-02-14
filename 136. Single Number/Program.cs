997b95ab2034228e2e982ece38147860e5de8e9f
jasur-abdulxaev <abdulkhaevjasur@gmail.com>
2026-02-15 01:20:06 +0500

    N9 tasks - LeetCode task added

---
+++ 136. Single Number/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+int[] nums = new[]
+{
+    1, 2, 3, 4, 5, 6,
+    1, 2, 4, 5, 6
+};
+
+int result = new Solution().SingleNumber(nums);
+Console.WriteLine(result);
+
+public class Solution
+{
+    public int SingleNumber(int[] nums)
+    {
+        int result = 0;
+        foreach (int num in nums)
+        {
+            result ^= num;
+        }
+        return result;
+    }
+}
