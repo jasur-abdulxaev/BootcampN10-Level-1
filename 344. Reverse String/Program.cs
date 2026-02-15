93cf393bd997eacc0e497de578b3920be8c401c0
jasur-abdulxaev <abdulkhaevjasur@gmail.com>
2026-02-16 00:23:45 +0500

    leetcode task added

---
+++ 344. Reverse String/Program.cs
@@ -0,0 +1,28 @@
+﻿
+char[] s = new char[]
+{
+    'A','b', 'b', 'o', 's'
+};
+
+
+
+Console.WriteLine(new string(s));
+
+Solution sol = new Solution();
+sol.ReverseString(s);
+Console.WriteLine(new string(s));
+
+public class Solution
+{
+    public void ReverseString(char[] s)
+    {
+        int left = 0, right = s.Length - 1;
+        while (left < right)
+        {
+            (s[left], s[right]) = (s[right], s[left]);
+            left++;
+            right--;
+        }
+    }
+}
+
---
+++ N8/Program.cs
@@ -0,0 +1,39 @@
+﻿#region  Pattern matching
+
+Console.WriteLine("Pattern matching");
+
+var value = (object)"false";
+
+Console.WriteLine(value.GetType);
+
+//Declarative Type Pattern matching - is, as
+Console.WriteLine("Declarative Pattern matching :  ");
+
+// Check and conversion in one
+if (value is int resultIntValue)
+{
+    Console.WriteLine(resultIntValue);
+}
+else if (value is string stringValue)
+{
+    Console.WriteLine(stringValue);
+}
+Console.WriteLine();
+
+
+//Relational Pattern matching
+Console.WriteLine("Relational Pattern matching");
+
+var grade = 80;
+var gradeLevel = grade switch
+{
+    >= 90 => "Top",
+    >= 80 and < 90 => "Good",
+    >= 70 and < 80 => "Normal",
+    _ => "Bad"
+};
+Console.WriteLine(gradeLevel);
+Console.WriteLine();
+
+#endregion
+
