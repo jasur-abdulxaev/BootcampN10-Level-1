f3c42f8eb2c425f3b60ec6318e4ab1e4436714b4
jasur-abdulxaev <abdulkhaevjasur@gmail.com>
2026-02-20 22:55:11 +0500

    N10 tasks added

---
+++ N10-T1/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Yoshingizni kiriting: ");
+int age = int.Parse(Console.ReadLine()!);
+
+try
+{
+    string message = CheckAge(age);
+    Console.WriteLine(message);
+}
+catch (ArgumentOutOfRangeException e)
+{
+    Console.WriteLine(e.Message);
+}
+
+static string CheckAge(int age)
+{
+    return age switch
+    {
+        < 18 => throw new ArgumentOutOfRangeException(nameof(age), "Sorry, you're too young"),
+        > 90 => throw new ArgumentOutOfRangeException(nameof(age), "Sorry, you're too old"),
+        _ => "Valid age"
+    };
+}
---
+++ N10-T2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+Random random = new Random();
+int secretNumber = random.Next(1, 11);
+
+Console.WriteLine("🎮 1 va 10 orasidagi sonni toping!");
+
+while (true)
+{
+    Console.Write("Sonni kiriting: ");
+    string input = Console.ReadLine()!;
+
+    try
+    {
+        // int.Parse o'zi FormatException throw qiladi
+        int guess = int.Parse(input);
+
+        // Noto'g'ri son bo'lsa ArgumentOutOfRangeException
+        if (guess != secretNumber)
+            throw new ArgumentOutOfRangeException(nameof(guess), "You couldn't guess it");
+
+        // To'g'ri topildi
+        Console.WriteLine("🎉 Congrats! You guessed it");
+        break;
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("❌ Not a number");
+    }
+    catch (ArgumentOutOfRangeException)
+    {
+        Console.WriteLine("❌ You couldn't guess it");
+    }
+}
