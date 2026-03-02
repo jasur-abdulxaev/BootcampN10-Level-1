3ad712e3021fc9fdc14b28ffa1f7a63fd1893ab6
jasur-abdulxaev <abdulkhaevjasur@gmail.com>
2026-03-02 23:06:13 +0500

    N13-T1 added

---
+++ N13-T1/Program.cs
@@ -0,0 +1,50 @@
+﻿public class Hero
+{
+    public int Id { get; }
+    public string Name { get; }
+
+    public Hero(int id, string name)
+    {
+        Id = id;
+        Name = name ?? throw new ArgumentNullException(nameof(name));
+    }
+
+    public override string ToString() => $"[{Id}] {Name}";
+}
+
+public class GameEngine
+{
+    protected readonly List<Hero> Heroes = new List<Hero>();
+
+    public GameEngine()
+    {
+        Heroes.Add(new Hero(1, "Yurnero"));
+        Heroes.Add(new Hero(2, "Sven"));
+        Heroes.Add(new Hero(3, "Tiny"));
+    }
+
+    public void Display()
+    {
+        foreach (var hero in Heroes)
+            Console.WriteLine(hero);
+    }
+}
+
+public class OptimizedGameEngine : GameEngine
+{
+    public OptimizedGameEngine()
+    {
+        Heroes.Add(new Hero(4, "Invoker"));
+        Heroes.Add(new Hero(5, "Lina"));
+        Heroes.Add(new Hero(6, "Medusa"));
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var engine = new OptimizedGameEngine();
+        engine.Display();
+    }
+}
